646a049faae87209296b9d3a2af51d213bebf560
zackharshal <harshalgotarne25@gmail.com>
2024-06-28 17:17:43 +0530

    Printed the maximum float value out of 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
-    public static Integer maxInt(Integer num1, Integer num2, Integer num3){
-        Integer max = num1;
+    public static Float maxInt(Float num1, Float num2, Float num3){
+        Float max = num1;
         if(num2.compareTo(max) > 0){
             max = num2;
         }
@@ -13,10 +13,10 @@ public class Main {
     }
     public static void main(String[] args) {
         //test case 1
-        System.out.println(maxInt(45, 12, 34));
+        System.out.println(maxInt(45.32f, 12.45f, 34.89f));
         //test case 2
-        System.out.println(maxInt(23, 89, 34));
+        System.out.println(maxInt(23.23f, 89.23f, 34.45f));
         //test case 3
-        System.out.println(maxInt(31, 56, 99));
+        System.out.println(maxInt(31.1f, 56f, 99.78f));
     }
 }
