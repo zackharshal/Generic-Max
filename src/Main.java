8890d5b5e6b6eb95ff98dacf35762baed6a60f82
zackharshal <harshalgotarne25@gmail.com>
2024-06-28 17:28:36 +0530

    Printed the maximum in the string out of 3

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 
 public class Main {
-    public static Float maxInt(Float num1, Float num2, Float num3){
-        Float max = num1;
-        if(num2.compareTo(max) > 0){
-            max = num2;
+    public static String maxString(String str1, String str2, String str3){
+        String max = str1;
+        if(str2.compareTo(max) > 0){
+            max = str2;
         }
-        if(num3.compareTo(max) > 0){
-            max = num3;
+        if(str3.compareTo(max) > 0){
+            max = str3;
         }
         return max;
 
     }
     public static void main(String[] args) {
         //test case 1
-        System.out.println(maxInt(45.32f, 12.45f, 34.89f));
+        System.out.println(maxString("Banana", "Apple", "Peach"));
         //test case 2
-        System.out.println(maxInt(23.23f, 89.23f, 34.45f));
+        System.out.println(maxString("Peach", "Banana", "Apple"));
         //test case 3
-        System.out.println(maxInt(31.1f, 56f, 99.78f));
+        System.out.println(maxString("Apple", "Peach", "Banana"));
     }
 }
