7f911122af3bcdef27a98489683b2d369096f266
zackharshal <harshalgotarne25@gmail.com>
2024-07-01 10:51:10 +0530

    Used options and sorting

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,38 @@
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 public class  Main <T extends Comparable<T>>{
-    private T str1;
-    private T str2;
-    private T str3;
-    Main(T str1, T str2, T str3) {
-        this.str1 = str1;
-        this.str2 = str2;
-        this.str3 = str3;
+    private T[] elements;
+
+    @SafeVarargs
+    Main(T... elements) {
+        this.elements = elements;
     }
     public void testMaximum(){
-        maximum(str1, str2, str3);
+        maximum(elements);
     }
-    public static <T extends Comparable<T>> void maximum(T str1, T str2, T  str3){
-        T max = str1;
-        if(str2.compareTo(max) > 0){
-            max = str2;
+    public static <T extends Comparable<T>> void maximum(T... elements){
+        if (elements == null || elements.length == 0) {
+            System.out.println("No elements provided");
+            return;
         }
-        if(str3.compareTo(max) > 0){
-            max = str3;
-        }
-        System.out.println(max);
+        List<T> list = Arrays.asList(elements);
+        Collections.sort(list);
+        T max = list.get(list.size() - 1);
+        System.out.println("Maximum value: " + max);
 
     }
     public static void main(String[] args) {
-        //test case 1
-        Main<Integer> obj1 = new Main<>(45, 56, 11);
+        // Test case 1
+        Main<Integer> obj1 = new Main<>(45, 56, 11, 99, 12);
         obj1.testMaximum();
 
-        Main<Float> obj2 = new Main<>(23f, 45f, 99f);
+        // Test case 2
+        Main<Float> obj2 = new Main<>(23f, 45f, 99f, 12f, 66f);
         obj2.testMaximum();
 
         // Test case 3
-        Main<String> obj3 = new Main<>("Peach", "Banana", "Apple");
+        Main<String> obj3 = new Main<>("Peach", "Banana", "Apple", "Mango");
         obj3.testMaximum();
     }
 }
